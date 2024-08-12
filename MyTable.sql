454a254e08a682d10e93467158cc52aa77eb6c7d
tyler-davis-sre <tylerd569@gmail.com>
2024-08-12 13:29:53 -0500

    Create MyTable.sql

---
+++ MyTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE MyTable
+(
+	first_name varchar(40),
+	last_name varchar(40), 
+	city varchar(40),
+	county varchar(40),
+	zip varchar(40),
+	officePhone varchar(40),
+	mobilePhone varchar(40),
+)
